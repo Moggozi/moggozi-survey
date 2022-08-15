ac49eaa33ffcc3121defc96642189c028d869ec4
sandsunset <sejoon0606@gmail.com>
2022-08-15 15:58:00 +0900

    feat: text input

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,16 +1,32 @@
+import { useState, useEffect } from 'react';
+
 import './Select.css';
 
 export default function Select(props) {
   const option = JSON.parse(props.option);
+  const [select, setSelect] = useState(null);
 
-  console.log(option);
+  useEffect(() => {
+    if (option === null) {
+      setSelect(
+        <div className="option">
+          <input></input>
+        </div>
+      )
+    } else {
+      const s = option.map((opt) => {
+        return (
+          <div key={ opt } className="option">
+            <div></div>
+            <p>{ opt }</p>
+          </div>
+        )
+      });
 
-  const select = option.map((opt) =>
-    <div key={ opt } className="option">
-      <div></div>
-      <p>{ opt }</p>
-    </div>
-  );
+      setSelect(s);
+    }
+    // eslint-disable-next-line
+  }, [option]);
 
   return (
     <div id="Select">
