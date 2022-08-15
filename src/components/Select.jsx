8a5c7b4d47f0707dc26861b7cebb9a4ad8656d14
sandsunset <sejoon0606@gmail.com>
2022-08-15 16:10:43 +0900

    feat: select

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,32 +1,31 @@
-import { useState, useEffect } from 'react';
-
 import './Select.css';
 
 export default function Select(props) {
   const option = JSON.parse(props.option);
-  const [select, setSelect] = useState(null);
 
-  useEffect(() => {
-    if (option === null) {
-      setSelect(
-        <div className="option">
-          <input></input>
+  function selectHandler(event) {
+    const target = event.target;
+    target.style.backgroundColor = "#FFCAA1"
+  }
+
+  var select;
+
+  if (option === null) {
+    select = (
+      <div className="option">
+        <input></input>
+      </div>
+    )
+  } else {
+    select = option.map((opt) => {
+      return (
+        <div key={ opt } className="option">
+          <div onClick={ selectHandler }></div>
+          <p>{ opt }</p>
         </div>
       )
-    } else {
-      const s = option.map((opt) => {
-        return (
-          <div key={ opt } className="option">
-            <div></div>
-            <p>{ opt }</p>
-          </div>
-        )
-      });
-
-      setSelect(s);
-    }
-    // eslint-disable-next-line
-  }, [option]);
+    });
+  }
 
   return (
     <div id="Select">
